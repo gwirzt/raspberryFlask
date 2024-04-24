ef396d9cbba40a3621671392202df781644fe6ce
Gustavo Wirzt <gwirzt@hotmail.com>
2024-04-24 19:07:18 -0300

    Menues

--- app.py
+++ app.py
@@ -1,30 +1,15 @@
 from flask import Flask, render_template, request, jsonify
 
-users = {
-    'gwirzt': {'password':'clave1'},
-    'psormani': {'password':'clave1'}
-}
+
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Página principal'
+    return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        
 
-        if username in users and users[username]['password'] == password :
-            return jsonify({'message': 'Inicio de sesión exitoso'})
-        else:
-            return jsonify({'message': 'Credenciales inválidas'}), 401
-    else:
-        return render_template('login.html')
 
 
 if __name__ == '__main__':
